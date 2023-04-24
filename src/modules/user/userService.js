1ac7ce4293a6f4057251cdae11d6d062cada104d
Jessica Lopes <lopes.jessica1988@gmail.com>
2023-04-24 20:27:40 -0300

    Criado tratativa de erros para signuo

--- src/modules/user/userService.js
+++ src/modules/user/userService.js
@@ -4,10 +4,8 @@ let users =[]
 const getUserByEmail = (searchEmail) => users.find((obj) => obj.email === searchEmail)
 
 export const signup = (data) => {
-  if (getUserByEmail(data.email)) {
-    console.log("EXISTE O EMAIL")
-  } else {
+  if (getUserByEmail(data.email)) throw new Error('email_existente')
+  
     users.push(data)
-  }
-  return true
+    return true
 }
